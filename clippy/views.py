e9e2d67929907214c92ad8eee8735452c130928d
Alicia Tang <xtang@linkedin.com>
2023-01-27 13:51:27 -0800

    chat sumbittion and render response

--- clippy/views.py
+++ clippy/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from .models import Question
-
 
 def index(request):
     question_list = Question.objects.order_by('pub_date')
@@ -9,3 +9,8 @@ def index(request):
         'question_list': question_list
     }
     return render(request, 'index.html', context)
+
+def chat(request):
+    data= {'answer': 'clippy says hi.'}
+    return JsonResponse(data)
+
